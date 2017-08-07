483f78a95a8ac4c298fe6e2bc31691b86e1f361b
Faeder-EN <faederdx@gmail.com>
2017-08-08 00:55:55 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
- 
+
 update() {
   git pull
   git submodule update --init --recursive
@@ -107,7 +107,6 @@ install() {
   cd ..
   install_luarocks
   install_rocks
-  
 }
 
 if [ "$1" = "install" ]; then
@@ -127,27 +126,28 @@ else
     exit 1
   fi
   
- ## chmod 777 config_fix.sh
- 
-   
-  if [ -f data/config.lua ]; then
-    ./config_fix.sh
-  fi
-  
-  if [ -f plugins/gban_installer.lua ]; then
-    
-    L=$(wc -l plugins/gban_installer.lua | cut -d " " -f1)
-    R=$(echo $L -20 | bc)
-    
-    #N=$(grep -nr "send_msg('chat#id'.*" plugins/gban_installer.lua | cut -d ":" -f1)
-    #M=$(grep -nr "send_msg('channel#id'.*" plugins/gban_installer.lua | cut -d ":" -f1)
-    
-    grep -v "send_msg('chat#id'.*" plugins/gban_installer.lua > gban1bot
-    grep -v "send_msg('channel#id'.*" gban1 > plugins/gban_installer.lua
-    sed -i "s/.*chat.*/&\n    send_msg('chat#id'..msg.to.id, '$R accounts globally banned. ☠', ok_cb, false)/" plugins/gban_installer.lua
-    sed -i "s/.*channel.*/&\n    send_msg('channel#id'..msg.to.id, '$R accounts globally banned. ☠', ok_cb, false)/" plugins/gban_installer.lua
-    rm gban1
-    
-  fi
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./Faeder-EN/Feader.lua -l 1 -E $@
+  chmod 777 faeder.sh
+
+    echo -e "\033[38;5;208m"   
+  echo -e "       CH > @Team_faeder            "
+  echo -e "       CH > @Team_faeder           "
+  echo -e "       CH > @Team_faeder    "
+  echo -e "       CH > @Team_faeder     "
+  echo -e "       CH > @Team_faeder      "
+  echo -e "       CH > @Team_faeder     "
+  echo -e "       CH > @Team_faeder            " 
+  echo -e "       CH > @Team_faeder            "     
+  echo -e "       CH > @Team_faeder           "
+  echo -e "       CH > @Team_faeder         " 
+  echo -e "       CH > @Team_faeder           " 
+  echo -e "       CH > @Team_faeder         \033[0;00m"
+  echo -e "\e[36m"                                      
+  echo -e "CH > @Team_faeder" 
+   cat << EOF
+EOF
+echo -e "\e[100m          CH > @Team_faeder        \e[00;37;40m"
+echo -e "\e[01;34m        CH > @Team_faeder         \e[00;37;40m"
+echo ""
+
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./Faeder/Faeder.lua -l 1 -E $@
 fi
