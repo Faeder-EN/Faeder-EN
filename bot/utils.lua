6b2bae20ce4ffb27bd35158876a2ea5e87561a09
Faeder-EN <faederdx@gmail.com>
2017-08-07 09:03:06 +0300

    Update utils.lua

--- bot/utils.lua
+++ bot/utils.lua
@@ -1218,3 +1218,46 @@ function ban_by_reply_admins(extra, success, result)
 		return
 	end
 end
+
+-- Unban by reply
+function unban_by_reply(extra, success, result)
+	if type(result) == 'boolean' then
+		print('Old message :(')
+		return false
+	end
+	if result.to.type == 'chat' or result.to.type == 'channel' then
+		local chat = 'chat#id'..result.to.peer_id
+		local channel = 'channel#id'..result.to.peer_id
+	if tonumber(result.from.peer_id) == tonumber(our_id) then -- Ignore bot
+		return
+	end
+		send_large_msg(chat, "User "..result.from.peer_id.." Unbanned")
+		-- Save on redis
+		local hash =  'banned:'..result.to.peer_id
+		redis:srem(hash, result.from.peer_id)
+	else
+		return
+  end
+end
+function banall_by_reply(extra, success, result)
+	if type(result) == 'boolean' then
+		print('Old message :(')
+		return false
+	end
+	if result.to.type == 'chat' or result.to.type == 'channel' then
+		local chat = 'chat#id'..result.to.peer_id
+		local channel = 'channel#id'..result.to.peer_id
+	if tonumber(result.from.peer_id) == tonumber(our_id) then -- Ignore bot
+		return
+	end
+	if is_admin2(result.from.peer_id) then -- Ignore admins
+		return
+	end
+		local name = user_print_name(result.from)
+		banall_user(result.from.peer_id)
+		chat_del_user(chat, 'user#id'..result.from.peer_id, ok_cb, false)
+		send_large_msg(chat, "User "..name.."["..result.from.peer_id.."] globally banned")
+	else
+		return
+  end
+end
