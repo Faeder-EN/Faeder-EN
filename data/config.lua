e7773c9efc227a8e958bec6bc322fce28289d8f4
Faeder-EN <faederdx@gmail.com>
2017-08-07 08:44:21 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,40 @@
+do local _ = {
+  about_text = "",
+  enabled_plugins = {
+    "plugins",
+    "super",
+    "write",
+    "AboutMe",
+    "pm",
+    "updater",
+    "setwelcome",
+    "stats",
+    "send",
+    "reply",
+    "red",
+    "name",
+    "mymsg",
+    "my",
+    "lock-flood",
+    "leave",
+    "inroom",
+    "help",
+    "gruop",
+    "delmsg",
+    "block",
+    "antyspam",
+    "admin",
+    "Devfaeder",
+    "banhammer"
+  },
+  help_text_realm = "",
+  help_text_super = "",
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    259142888
+  }
+}
+return _
+end
